64b7487102685fddd0020e68ef467e75a5d8b4ac
anikaa22 <153383035+anikaa22@users.noreply.github.com>
2025-04-16 21:14:24 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-from drowsiness_detection.drowsiness_detection import detect_drowsiness_from_video
-from drowsiness_detection.drowsiness_detection import detect_drowsiness_from_webcam
-from speech_detection.speech_detection import monitor_speech
 
-# Run speech monitoring for 30 seconds
-monitor_speech(duration_seconds=30)
-is_drowsy = detect_drowsiness_from_webcam( display=True)
-print("Drowsiness Detected" if is_drowsy else "Driver was alert")
